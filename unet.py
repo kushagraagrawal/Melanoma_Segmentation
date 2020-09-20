a54734efddddaf1385669125b482115502702ef8
Kushagra Agrawal <kushagra106@gmail.com>
2020-09-20 16:41:12 +0530

    changed unet to tf.keras, have to figure out training now

--- unet.py
+++ unet.py
@@ -2,92 +2,99 @@ from __future__ import print_function
 
 import cv2
 import numpy as np
-from keras import backend as K
-from keras.layers import Input, Concatenate, Conv2D, MaxPooling2D, UpSampling2D, Dropout
-from keras.models import Model
-from keras.optimizers import Adam
+import tensorflow as tf
+# from keras import backend as K
+# from keras.layers import Input, Concatenate, Conv2D, MaxPooling2D, UpSampling2D, Dropout
+# from keras.models import Model
+# from keras.optimizers import Adam
 
 from constants import img_rows, img_cols
 
-# K.set_image_data_format('th')
+# tf.enable_eager_execution()
+tf.keras.backend.set_image_data_format('channels_first')
 
 smooth = 1
 
 def dice_coef(y_true, y_pred):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    y_true_f = tf.keras.backend.flatten(y_true)
+    y_pred_f = tf.keras.backend.flatten(y_pred)
+    intersection = tf.keras.backend.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (tf.keras.backend.sum(y_true_f) + tf.keras.backend.sum(y_pred_f) + smooth)
 
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
-
+    
+@tf.function
 def call(self, inputs, training=None):
     if 0. < self.rate < 1.:
         noise_shape = self._get_noise_shape(inputs)
         def dropped_inputs():
-            return K.dropout(inputs, self.rate, noise_shape, seed=self.seed)
+            return tf.keras.backend.dropout(inputs, self.rate, noise_shape, seed=self.seed)
         if(training):
-            return K.in_train_phase(dropped_inputs, inputs, training=training)
+            return tf.keras.backend.in_train_phase(dropped_inputs, inputs, training=training)
         else:
-            return K.in_test_phase(dropped_inputs, inputs, training=None)
+            return tf.keras.backend.in_test_phase(dropped_inputs, inputs, training=None)
     return inputs
 
-Dropout.call = call
+tf.keras.layers.Dropout.call = call
 
+# 
 def get_unet(dropout):
-    inputs = Input((img_rows, img_cols,1))
-    conv1 = Conv2D(32, 3, activation='relu', padding='same',input_shape=inputs.shape)(inputs)
-    conv1 = Conv2D(32, 3, activation='relu', padding='same')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    inputs = tf.keras.layers.Input((1, img_rows, img_cols))
+    conv1 = tf.keras.layers.Conv2D(32, 3, activation='relu', padding='same',input_shape=inputs.shape)(inputs)
+    conv1 = tf.keras.layers.Conv2D(32, 3, activation='relu', padding='same')(conv1)
+    pool1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv1)
 
 
-    conv2 = Conv2D(64, 3, activation='relu', padding='same')(pool1)
-    conv2 = Conv2D(64, 3, activation='relu', padding='same')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv2 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same')(pool1)
+    conv2 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same')(conv2)
+    pool2 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv2)
 
    
-    conv3 = Conv2D(128, 3, activation='relu', padding='same')(pool2)
-    conv3 = Conv2D(128, 3, activation='relu', padding='same')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv3 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same')(pool2)
+    conv3 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same')(conv3)
+    pool3 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv3)
 
-    conv4 = Conv2D(256, 3, activation='relu', padding='same')(pool3)
-    conv4 = Conv2D(256, 3, activation='relu', padding='same')(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    conv4 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same')(pool3)
+    conv4 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same')(conv4)
+    pool4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv4)
    
-    conv5 = Conv2D(512, 3, activation='relu', padding='same')(pool4)
-    conv5 = Conv2D(512, 3, activation='relu', padding='same')(conv5)
+    conv5 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same')(pool4)
+    conv5 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same')(conv5)
 
     if dropout:
-        conv5 = Dropout(0.5)(conv5)    
+        conv5 = tf.keras.layers.Dropout(0.5)(conv5)    
 
-    up6 = Concatenate(axis=-1)([UpSampling2D(size=(2, 2))(conv5), conv4])
+    up6 = tf.keras.layers.Concatenate(axis=1)([tf.keras.layers.UpSampling2D(size=(2, 2))(conv5), conv4])
 
-    conv6 = Conv2D(256, 3, activation='relu', padding='same')(up6)
-    conv6 = Conv2D(256, 3, activation='relu', padding='same')(conv6)
+    conv6 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same')(up6)
+    conv6 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same')(conv6)
 
-    up7 = Concatenate(axis=-1)([UpSampling2D(size=(2, 2))(conv6), conv3])
+    up7 = tf.keras.layers.Concatenate(axis=1)([tf.keras.layers.UpSampling2D(size=(2, 2))(conv6), conv3])
 
-    conv7 = Conv2D(128, 3, activation='relu', padding='same')(up7)
-    conv7 = Conv2D(128, 3, activation='relu', padding='same')(conv7)
+    conv7 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same')(up7)
+    conv7 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same')(conv7)
 
-    up8 = Concatenate(axis=-1)([UpSampling2D(size=(2, 2))(conv7), conv2])
+    up8 = tf.keras.layers.Concatenate(axis=1)([tf.keras.layers.UpSampling2D(size=(2, 2))(conv7), conv2])
 
-    conv8 = Conv2D(64, 3, activation='relu', padding='same')(up8)
-    conv8 = Conv2D(64, 3, activation='relu', padding='same')(conv8)
+    conv8 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same')(up8)
+    conv8 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same')(conv8)
 
-    up9 = Concatenate(axis=-1)([UpSampling2D(size=(2, 2))(conv8), conv1])
+    up9 = tf.keras.layers.Concatenate(axis=1)([tf.keras.layers.UpSampling2D(size=(2, 2))(conv8), conv1])
 
-    conv9 = Conv2D(32, 3, activation='relu', padding='same')(up9)
-    conv9 = Conv2D(32, 3, activation='relu', padding='same')(conv9)
+    conv9 = tf.keras.layers.Conv2D(32, 3, activation='relu', padding='same')(up9)
+    conv9 = tf.keras.layers.Conv2D(32, 3, activation='relu', padding='same')(conv9)
 
-    conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
+    conv10 = tf.keras.layers.Conv2D(1, 1, activation='sigmoid')(conv9)
 
-    model = Model(input=inputs, output=conv10)
+    model = tf.keras.Model(inputs=inputs, outputs=conv10)
 
-    model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
+    my_loss= dice_coef_loss
+    my_metric = dice_coef
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(lr=1e-5), loss=my_loss, metrics=[my_metric])
 
     return model
 
 if __name__ == "__main__":
-    model = get_unet(True)
+    get_unet(True)
